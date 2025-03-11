ad699d7b4c9c576fc74c8133b1f4c99d8914c7e9
thanusree-123 <148942627+thanusree-123@users.noreply.github.com>
2025-03-11 23:41:19 +0530

    Update App.js

--- frontend/pages/App.js
+++ frontend/pages/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import ProductList from "./components/ProductList";
-import Cart from "./components/Cart";
-import ProductForm from "./components/ProductForm";
+import ProductList from "../components/ProductList";
+import Cart from "../components/Cart";
+import ProductForm from "../components/ProductForm";
 
 function App() {
   const [products, setProducts] = useState([]);
